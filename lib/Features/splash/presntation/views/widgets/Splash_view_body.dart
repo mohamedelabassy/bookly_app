8f3218a7caff76b8056d0454b39bba7bba6aaa37
mohamedelabassy <mohamedelabasy144@gmail.com>
2023-07-19 19:46:08 +0300

    edit NavigateToHome

--- lib/Features/splash/presntation/views/widgets/Splash_view_body.dart
+++ lib/Features/splash/presntation/views/widgets/Splash_view_body.dart
@@ -61,8 +61,9 @@ class _SplashViewBodyState extends State<SplashViewBody>
   }
 
   void navigateToHome() {
-    Future.delayed(const Duration(seconds: 3));
-    Get.to(() => const HomeView(),
-        transition: Transition.fade, duration: KTranstionDuration);
+    Future.delayed(Duration(seconds: 3), () {
+      Get.to(() => const HomeView(),
+          transition: Transition.fade, duration: KTranstionDuration);
+    });
   }
 }
