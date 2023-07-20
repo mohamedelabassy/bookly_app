70d907aeae4b1ded3e1c6b7ce177c136a5469f07
mohamedelabassy <mohamedelabasy144@gmail.com>
2023-07-21 01:50:23 +0300

    BestSellerListViewItem part 1

--- lib/Features/home/presntation/views/widgets/Home_view_body.dart
+++ lib/Features/home/presntation/views/widgets/Home_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/style.dart';
 import 'package:flutter/material.dart';
 
@@ -22,6 +23,37 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Style.titleMeduim,
+          ),
+          BestSellerListViewItem()
+        ],
+      ),
+    );
+  }
+}
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+          AspectRatio(
+            aspectRatio: 2.5 / 4,
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.red,
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(
+                    AssetsData.testImage,
+                  ),
+                ),
+              ),
+            ),
           )
         ],
       ),
