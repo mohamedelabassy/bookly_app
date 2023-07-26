947d424354b0ff4e32b9273d7166f8dd186ab908
mohamedelabassy <mohamedelabasy144@gmail.com>
2023-07-26 19:42:32 +0300

    create aoi services

---
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/Features/home/data/Models/book_model/book_model.dart';
+import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
+class HomeRepoImpl implements HomeRebo {
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
+    // TODO: implement fetchBestSellerBooks
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks() {
+    // TODO: implement fetchFeatureBooks
+    throw UnimplementedError();
+  }
+}
