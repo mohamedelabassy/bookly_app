792b897d47a55a078f1cbcbbfed77523997fa3f6
mohamedelabassy <mohamedelabasy144@gmail.com>
2023-10-10 02:18:26 +0300

    implement news books

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -2,7 +2,7 @@ import 'package:bookly_app/Features/home/data/Models/book_model/book_model.dart'
 import 'package:bookly_app/core/errors/failures.dart';
 import 'package:dartz/dartz.dart';
 
-abstract class HomeRebo {
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks();
   Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
 }
--- lib/Features/home/data/repos/home_repo_impl.dart
+++ lib/Features/home/data/repos/home_repo_impl.dart
@@ -1,13 +1,28 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:dartz/dartz.dart';
+
 import 'package:bookly_app/Features/home/data/Models/book_model/book_model.dart';
 import 'package:bookly_app/Features/home/data/repos/home_repo.dart';
 import 'package:bookly_app/core/errors/failures.dart';
-import 'package:dartz/dartz.dart';
+import 'package:bookly_app/core/utils/api_services.dart';
 
-class HomeRepoImpl implements HomeRebo {
+class HomeRepoImpl implements HomeRepo {
+  ApiServices apiServices;
+  HomeRepoImpl(this.apiServices);
   @override
-  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchNewsetBooks() async {
+    try {
+      var data = await apiServices.get(
+          endPoint:
+              'volumes?filtering=free_ebooks&sorting=newest&q=subject: Programing');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(BookModel.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServreFaliure());
+    }
   }
 
   @override
